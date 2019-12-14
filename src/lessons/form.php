cbb5609313bbf3d2143e2b5216291865c3eff8c8
maniak <djmaniakfeatmcrybik@gmail.com>
2019-12-14 18:28:12 +0200

    message

--- src/lessons/form.php
+++ src/lessons/form.php
@@ -20,23 +20,31 @@ if ($text) {
 foreach ($array as $value) {
     $LoginComparison = $value[0];
     $PasswordComparison = $value[1];
-
+    $i = 1;
     if (trim($login) === trim($LoginComparison) and trim($password) === trim($PasswordComparison)) {
-        echo "Ваш логин: $LoginComparison";
-        var_dump($LoginComparison);die;
-    } else {
-        Header('Location: /lessons/index.php'); die;
-    };
+       echo  "<h1> Добро пожаловать: $LoginComparison</h1>";
+        $FileGet = file_get_contents($LoginComparison . ".txt");
+        if (isset($FileGet)) {
+            $i = $FileGet + 1;
+            file_put_contents($LoginComparison . ".txt", $i);
+            echo "Количество входа на сайт: $i ";die;
+        }else{
+            file_put_contents($LoginComparison . ".txt", $i);die;
+        }
 
-};
+    }
+}
 
 
 /*
- * $fp = fopen($login . ".txt", "w"); // Создает новый файл в режиме записи
-        $mytext = "Для наглядности"; // Исходная строка
-        fputs($fp, $mytext); // Запись в файл
-        fclose($fp); //Закрытие файлає
- */
+$fp = fopen($login . ".txt", "w"); // Создает новый файл в режиме записи
+$mytext = "Для наглядности"; // Исходная строка
+fputs($fp, $mytext); // Запись в файл
+fclose($fp); //Закрытие файлає
+*/
+
+Header('Location: /lessons/index.php');
+die;
 
 
 ?>
